0e6f0ef02b5e469a6f467763d8b07b309adca630
minD <tuyetnhi1607@github.com>
2024-06-14 15:44:59 +0700

    chapter 5

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface ICustomersProps {
+}
+
+export default function Customers (props: ICustomersProps) {
+  return (
+    <div>
+      <p>Customers Page</p>
+    </div>
+  );
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface IInvoicesProps {
+}
+
+export default function Invoices(props: IInvoicesProps) {
+  return (
+    <div>
+      <p>Invoices Page</p>
+    </div>
+  );
+}
