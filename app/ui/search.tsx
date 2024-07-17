d1b95ae08678dd2edde465e652c1d6a50d089d83
minD <tuyetnhi1607@github.com>
2024-07-17 11:22:34 +0700

    ud

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,21 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams(); // get search params
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  function handleSearch(term: string) {
+    const params = new URLSearchParams(searchParams); // create a new URLSearchParams object from the current search params
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +24,15 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        value={searchParams.get('query') || ''} 
+        // why defaultValue instead of value? 
+        // because the value attribute is used to set the initial value of the input, 
+        // and we want to set the initial value to the value of the query parameter
+        // the defaultValue attribute is used to set the initial value of the input,
+        // and we want to set the initial value to the value of the query parameter
+        // if we use the value attribute, the input will be uncontrolled and the user won't be able to type in it
+        id="search"
+        onChange={(e) => handleSearch(e.target.value)}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
