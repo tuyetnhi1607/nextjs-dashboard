df6d0c263dcd60a8466ac5fd00fb19470f725fe1
minD <tuyetnhi1607@github.com>
2024-07-18 03:20:30 +0700

    chapter 13

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,21 +1,25 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { usePathname, useSearchParams, useRouter } from 'next/navigation';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
-  const searchParams = useSearchParams(); // get search params
+  const searchParams = useSearchParams(); // get search params, read-only
   const pathname = usePathname();
   const { replace } = useRouter();
-  function handleSearch(term: string) {
-    const params = new URLSearchParams(searchParams); // create a new URLSearchParams object from the current search params
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
+
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -24,9 +28,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        value={searchParams.get('query') || ''} 
-        // why defaultValue instead of value? 
-        // because the value attribute is used to set the initial value of the input, 
+        defaultValue={searchParams.get('query')?.toString()}
+        // why defaultValue instead of value?
+        // because the value attribute is used to set the initial value of the input,
         // and we want to set the initial value to the value of the query parameter
         // the defaultValue attribute is used to set the initial value of the input,
         // and we want to set the initial value to the value of the query parameter
